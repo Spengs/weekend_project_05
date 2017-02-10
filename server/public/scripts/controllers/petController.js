b424caf011174c72f2dc4152bde2b2b1e2fa8074
Spengs <Jotto465@gmail.com>
2017-02-10 12:24:14 -0600

    drop down

--- server/public/scripts/controllers/petController.js
+++ server/public/scripts/controllers/petController.js
@@ -1,4 +1,39 @@
 myApp.controller('PetsController', ['$scope', '$http',
 function ($scope, $http) {
   console.log('pets');
-}])
+  $scope.petTypes = [
+    {
+      name: 'Barn Yard',
+      type: 'barnyard',
+   },
+   {
+     name: 'Birds',
+     type: 'bird',
+  },
+  {
+    name: 'Cats',
+    type: 'cat',
+  },
+   {
+     name: 'Dogs',
+     type: 'dog',
+  },
+  {
+    name: 'Horses',
+    type: 'horse',
+  },
+  {
+    name: 'Pigs',
+    type: 'pig',
+  },
+  {
+    name: 'Reptiles',
+    type: 'reptile',
+  },
+  {
+    name: 'Small Furrys',
+    type: 'smallfurry',
+  }
+];
+console.log($scope.petTypes[0].name);
+}]);
